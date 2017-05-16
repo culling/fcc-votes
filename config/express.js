88e256976196f3b026a65eaa21cd62ac8cea2eb0
culling <culling66@gmail.com>
2017-05-17 04:35:32 +0800

    clean passport for local

--- config/express.js
+++ config/express.js
@@ -1,15 +1,16 @@
+//Settings
 var config      = require("./config");
+var passport    = require("./passport");
+var mongo       = require("./mongo");
+
+//Modules
 var http        = require('http');
 var express     = require("express");
 var cookieParser    = require('cookie-parser');
 var expressSession  = require('express-session');
-var passport    = require("./passport");
+
+//express app
 var app         = express();
-
-
-//var passport    = require('passport');
-var mongo       = require("./mongo");
-var Strategy    = require('passport-local').Strategy;
 
 
 module.exports = function(){
@@ -19,45 +20,22 @@ module.exports = function(){
     app.use(cookieParser());
     app.use(expressSession({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
     
-    passport.use(new Strategy(
-    function(username, password, cb) {
-        console.log("local strategy called");
-        mongo.users.findByUsername(username, function(err, user) {
-        if (err) { return cb(err); }
-        if (!user) { return cb(null, false); }
-        if (user.password != password) { return cb(null, false); }
-        return cb(null, user);
-        });
-    }));
-
-    passport.serializeUser(function(user, cb) {
-        console.log("serializedUser called");        
-        cb(null, user.id);
-    });
-
-    passport.deserializeUser(function(id, cb) {
-        console.log("deserializedUser called");        
-        mongo.users.findById(id, function (err, user) {
-            if (err) { return cb(err); }
-            cb(null, user);
-        });
-    });
-    
     app.use(passport.initialize());
     app.use(passport.session());
 
-//Views
-app.set("views", "./app/views");
-app.set("view engine", "ejs");
+    //Views
+    app.set("views", "./app/views");
+    app.set("view engine", "ejs");
 
-var api = require("./../app/routes/api.route.server");
-app.use("/api", api);
+    //Routes
+    var api = require("./../app/routes/api.route.server");
+    app.use("/api", api);
 
-var index = require("./../app/routes/index.route.server");
-app.use("/", index);
+    var index = require("./../app/routes/index.route.server");
+    app.use("/", index);
 
-//static files
-app.use(express.static('./public'));
+    //static files
+    app.use(express.static('./public'));
 
-return server;
+    return server;
 }
--- config/passport.js
+++ config/passport.js
@@ -2,33 +2,33 @@ var passport    = require('passport');
 var mongo       = require("./mongo");
 var Strategy    = require('passport-local').Strategy;
 
-    passport.use(new Strategy(
-    function(username, password, cb) {
-        console.log("local strategy called");
-        mongo.users.findByUsername(username, function(err, user) {
+passport.use(new Strategy(
+function(username, password, cb) {
+    console.log("local strategy called");
+    mongo.users.findByUsername(username, function(err, user) {
+    if (err) { return cb(err); }
+    if (!user) { return cb(null, false); }
+    if (user.password != password) { return cb(null, false); }
+    return cb(null, user);
+    });
+}));
+
+passport.serializeUser(function(user, cb) {
+    console.log("serializedUser called");        
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+    console.log("deserializedUser called");        
+    mongo.users.findById(id, function (err, user) {
         if (err) { return cb(err); }
-        if (!user) { return cb(null, false); }
-        if (user.password != password) { return cb(null, false); }
-        return cb(null, user);
-        });
-    }));
-
-    passport.serializeUser(function(user, cb) {
-        console.log("serializedUser called");        
-        cb(null, user.id);
+        cb(null, user);
     });
-
-    passport.deserializeUser(function(id, cb) {
-        console.log("deserializedUser called");        
-        mongo.users.findById(id, function (err, user) {
-            if (err) { return cb(err); }
-            cb(null, user);
-        });
-    });
+});
 
 
 
-module.exports  = passport
+module.exports = passport;
 /*
 module.exports  = function(){
     //require('./strategies/local')();
